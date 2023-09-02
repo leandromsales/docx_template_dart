0027bde8bc2306e893361f7b8b0529e7e301f5e4
Leandro Melo de Sales <leandromsales@gmail.com>
2023-09-02 19:40:00 -0300

    Fetch upstream and cherry-pick alexandre

--- fetch_upstream.sh
+++ fetch_upstream.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-UPSTREAM_URL="git@github.com:PavelS0/docx_template_dart.git"
+PAVEL_REMOTE_ORIGIN="upstream"
+PAVEL_URL="git@github.com:PavelS0/docx_template_dart.git"
+ALEXANDRE_REMOTE_ORIGIN="alexandre"
+ALEXANDRE_URL="git@github.com:AlexandreMaillot/docx_template_dart.git"
+
+add_remote_origin() {
+	name=$1
+	url=$2
+	if ! git remote | grep -q "^${name}$"; then
+		git remote add upstream $url
+	fi
+}
 
 CURR_DIR=`pwd`
 SCRIPT_SYMLINK_DIR=`readlink $0`
@@ -13,11 +24,33 @@ fi
 
 cd "$SCRIPTDIR"
 
-if ! git remote | grep -q '^upstream$'; then
-	git remote add upstream $UPSTREAM_URL
+if command -v tac &>/dev/null; then
+    REVERSE_CMD="tac"
+elif command -v tail &>/dev/null; then
+    REVERSE_CMD="tail -r"
+else
+    echo "Neither tac nor tail is available. Exiting."
+    exit 1
 fi
 
-git fetch upstream
+add_remote_origin $PAVEL_REMOTE_ORIGIN $PAVEL_URL
+git fetch $PAVEL_REMOTE_ORIGIN
+
+add_remote_origin $ALEXANDRE_REMOTE_ORIGIN $ALEXANDRE_URL
+git fetch $ALEXANDRE_REMOTE_ORIGIN
+
+git config --global pager.log false
+
+(git log --pretty=format:"%H" ${PAVEL_REMOTE_ORIGIN}/master..${ALEXANDRE_REMOTE_ORIGIN}/master; echo "") | while read commit; do
+	if [ ! -z "$commit" ]; then
+		if ! git merge-base --is-ancestor $commit HEAD; then
+			echo "Cherrying pick commit $commit"
+			#git cherry-pick $commit
+		fi
+	fi
+done
+
+exit
 
 git checkout master
 
